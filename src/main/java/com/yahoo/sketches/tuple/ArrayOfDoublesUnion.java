682a816852d697dc4de598ad29929d7bba2b473b
saydakov <saydakov@yahoo-inc.com>
2015-12-28 11:44:43 -0800

    added copyright

--- src/main/java/com/yahoo/sketches/tuple/ArrayOfDoublesUnion.java
+++ src/main/java/com/yahoo/sketches/tuple/ArrayOfDoublesUnion.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright 2015, Yahoo! Inc.
+ * Licensed under the terms of the Apache License 2.0. See LICENSE file at the project root for terms.
+ */
 package com.yahoo.sketches.tuple;
 
 public abstract class ArrayOfDoublesUnion {

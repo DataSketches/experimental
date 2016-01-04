e06fe5559fedd94ea0ec101867bffe61b7caaf80
Lee Rhodes <lrhodes@yahoo-inc.com>
2016-01-04 12:06:28 -0800

    stop printing from HeapQuantilesSketchTest

--- src/test/java/com/yahoo/sketches/quantiles/HeapQuantilesSketchTest.java
+++ src/test/java/com/yahoo/sketches/quantiles/HeapQuantilesSketchTest.java
@@ -399,7 +399,7 @@ public class HeapQuantilesSketchTest {
    * @param s value to print 
    */
   static void println(String s) {
-    System.out.println(s); //disable here
+    //System.out.println(s); //disable here
   }
   
 }

b4fc949a8bdf5dc6a152f4f2e0f6de0d08432e79
Edo Liberty <edo@yahoo-inc.com>
2016-06-06 09:08:50 -0400

    corrected bug

--- experiment/make_data.py
+++ experiment/make_data.py
@@ -3,17 +3,15 @@ import numpy
 
 n = 10000000
 
-fout = open('data/exponential_n=%d_beta=%d.csv'%(n,beta),'w')
 beta = 1000
-sys.stderr.write('Creating exponential data\n')
+fout = open('data/exponential_n=%d_beta=%d.csv'%(n,beta),'w')
 for i in range(n):        
     x = numpy.random.exponential(beta)
     fout.write('%d\n'%x)
 fout.close()
 
-fout = open('data/zipfian_n=%d_alpha=%f.csv'%(n,alpha),'w')
 alpha = 1.001
-sys.stderr.write('Creating zipfian data\n')
+fout = open('data/zipfian_n=%d_alpha=%f.csv'%(n,alpha),'w')
 for i in range(n):
     x = int(numpy.random.zipf(alpha))
     fout.write('%d\n'%x)
@@ -25,7 +23,6 @@ for i in range(n):
     fout.write('%d\n'%x)
 fout.close()
 
-sys.stderr.write('Creating planted data\n')
 k = 1000
 fout = open('data/planted_n=%d_k=%d.csv'%(n,k),'w')
 for i in range(n):

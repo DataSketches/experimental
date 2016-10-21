0465e22160f5885d732033ea8f296b5524a2b62d
saydakov <saydakov@yahoo-inc.com>
2016-10-21 14:51:29 -0700

    configurable ininial size

--- src/main/java/com/yahoo/sketches/hllmap/CmdLine.java
+++ src/main/java/com/yahoo/sketches/hllmap/CmdLine.java
@@ -19,8 +19,12 @@ public class CmdLine {
    * @throws Exception exception thrown
    */
   public static void main(String[] args) throws Exception {
+    int initNumEntries = 100_000_000;
+    if (args.length > 0) {
+      initNumEntries = Integer.parseInt(args[0]);
+    }
     String itemStr = "";
-    UniqueCountMap map = new UniqueCountMap(100000000, 4, 1024);
+    UniqueCountMap map = new UniqueCountMap(initNumEntries, 4, 1024);
     UpdateSketch sketch = UpdateSketch.builder().setNominalEntries(65536).build();
     long count = 0;
     long updateTimeNs = 0;
